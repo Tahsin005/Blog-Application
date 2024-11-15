89d054bba7053cbf4a23ae7a4b673f530dfb9001
tahsin005 <tahsin.ferdous3546@gmail.com>
2024-11-16 04:14:59 +0600

    added canonical url for blog post object

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 from django.db import models
+from django.urls import reverse
 from django.utils import timezone
 # Create your models here.
 class PublishedManager(models.Manager):
@@ -39,3 +40,9 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse(
+            'blog:post_detail',
+            args=[self.id]
+        )
